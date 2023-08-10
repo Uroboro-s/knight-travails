783d42188f5a2728f01537eca1782905d6c9328a
Janmeyjai Mishra <mishrajanmeyjai100@gmail.com>
2023-08-10 15:10:16 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import intermmediateFunction from "./tree.js";
+
+console.log("hello");
+let start_coordinates = [1,1];
+let end_coordinates = [8, 8];
+
+
+intermmediateFunction(start_coordinates, end_coordinates);
